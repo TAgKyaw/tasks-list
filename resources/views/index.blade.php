4fe9e7ce7590f3963f2fb389fc41bf4d4d0a7ef4
TAgKyaw <dannynaing727@gmail.com>
2024-06-08 00:50:41 +0100

    test commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+    @section('title','Task List')
+    <!-- @isset($name)
+    <h1>My name is {{$name}}</h1>
+    @endisset -->
+    @section('content')
+    <!-- @if (count($tasks))
+            @foreach ($tasks as $task)
+            <div>
+                {{$task->title}}
+            </div>
+            @endforeach
+        @else
+            <div>
+                There are no tasks
+            </div>
+        @endif -->
+        @forelse ($tasks as $task )
+        <div>
+            <a href="{{ route('tasks.show', ['id'=> $task->id ]) }}">{{$task->title}}</a>
+        </div>
+        @empty
+        <div>
+            There are no tasks
+        </div>
+        @endforelse
+    @endsection
+
+
+
+
